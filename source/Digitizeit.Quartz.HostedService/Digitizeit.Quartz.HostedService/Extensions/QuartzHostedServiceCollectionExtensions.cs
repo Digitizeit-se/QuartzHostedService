f98137908c6bdd9dc554241d4f4a81b926d1205b
John Larsen <john@pcis.se>
2020-04-01 20:49:36 +0200

    Added Service collection extension

---
+++ source/Digitizeit.Quartz.HostedService/Digitizeit.Quartz.HostedService/Extensions/QuartzHostedServiceCollectionExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Digitizeit.Quartz.HostedService.Factory;
+using Digitizeit.Quartz.HostedService.Interfaces;
+using Digitizeit.Quartz.HostedService.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Quartz.Impl;
+using Quartz.Spi;
+using System;
+
+namespace Digitizeit.Quartz.HostedService.Extensions
+{
+    public static class QuartzHostedServiceCollectionExtensions
+    {
+        public static IServiceCollection AddQuartzHostedService(this IServiceCollection services, IConfiguration config)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            services.AddOptions();
+
+            //database services
+            services.AddTransient<QuartzSqlServerOption>();
+            services.AddTransient<QuartzSqliteOptions>();
+            services.AddTransient<QuartzMySqlOptions>();
+            services.AddTransient<IDatabaseCreatorFactory, DatabaseCreatorFactory>();
+
+            services.AddSingleton<IJobFactory, JobFactory>();
+            services.AddSingleton(provider =>
+            {
+                var option = new QuartzBaseOptions(config, services, provider.GetService<DatabaseCreatorFactory>(), provider.GetService<ILogger<QuartzBaseOptions>>());
+                var sf = new StdSchedulerFactory(option.ToProperties());
+                var scheduler = sf.GetScheduler().Result;
+                scheduler.JobFactory = provider.GetService<IJobFactory>();
+                return scheduler;
+            });
+
+            services.AddHostedService<QuartzHostedService>();
+
+            return services;
+        }
+    }
+}

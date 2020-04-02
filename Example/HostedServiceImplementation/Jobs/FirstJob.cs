6043dadd605ec2b540e0fd3c743f1aa58a8b1028
John Larsen <john@pcis.se>
2020-04-02 10:18:32 +0200

    Added a implementation Example

---
+++ Example/HostedServiceImplementation/Jobs/FirstJob.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Quartz;
+
+namespace HostedServiceImplementation.Jobs
+{
+    public class FirstJob : IJob
+    {
+        private readonly ILogger _logger;
+
+        public FirstJob(ILogger<FirstJob> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("FirstJob is running...");
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- Example/HostedServiceImplementation/Program.cs
+++ Example/HostedServiceImplementation/Program.cs
@@ -1,12 +1,87 @@
-﻿using System;
+﻿using Digitizeit.Quartz.HostedService.Extensions;
+using HostedServiceImplementation.Jobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.IO;
 
 namespace HostedServiceImplementation
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static int Main()
         {
-            Console.WriteLine("Hello World!");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Debug)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Building host.");
+                var host = BuildHost()
+                    .Build();
+
+                Log.Information("Starting host");
+                host.Run();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
+
+        private static IHostBuilder BuildHost()
+        {
+            var hostBuilder = new HostBuilder();
+
+            hostBuilder.UseEnvironment(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Debug")
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                })
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    configApp.AddJsonFile("appsettings.json", true);
+                })
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    configApp.AddJsonFile($"appsettings.{ProviderSwitch.Memory.ToString()}.json",
+                        true);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddQuartzHostedService(hostContext.Configuration);
+                    services.AddSingleton<FirstJob, FirstJob>();
+                })
+                .UseConsoleLifetime()
+                .UseSerilog();
+            return hostBuilder;
+        }
+    }
+
+    /// <summary>
+    /// This is used to switch what application.**.Json file to use.
+    /// You need to run the right docker container for this to work if choice is a database provider
+    /// change this row   configApp.AddJsonFile($"appsettings.{ProviderSwitch.Memory.ToString()}.json", above to desired database provider
+    /// </summary>
+    public enum ProviderSwitch
+    {
+        Memory,
+        SqLite,
+        SqlServer,
+        MySql
     }
 }
